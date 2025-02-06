d6ff1c459b64882937fad022ec2e87ef34fab69c
Durga sri niharika <142823558+Durgasriniharika@users.noreply.github.com>
2025-02-06 11:56:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Reverse Array Using Stack/reverse-array-using-stack.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+// Complete this function
+class Solution {
+  public:
+    void reverseArray(vector<int>& arr) {
+        stack<int>s;
+        for(int i=0;i<arr.size();i++){
+            s.push(arr[i]);
+        }
+        for(int i=0;i<arr.size();i++){
+            arr[i]=s.top();
+            s.pop();
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> arr;
+        int num;
+        while (ss >> num) {
+            arr.push_back(num);
+        }
+        Solution obj;
+        obj.reverseArray(arr);
+
+        for (int i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+
+        cout << endl;
+        cout << "~\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
