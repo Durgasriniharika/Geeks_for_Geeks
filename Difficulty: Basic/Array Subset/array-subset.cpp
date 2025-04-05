bf06918f0eb81f832cf92f0e1f05bffbc1463561
Durga sri niharika <142823558+Durgasriniharika@users.noreply.github.com>
2025-04-05 21:52:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Subset/array-subset.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    // Function to check if b is a subset of a
+    bool isSubset(vector<int> &a, vector<int> &b) {
+        unordered_map<int,int>mp;
+        for(auto it:a){
+            mp[it]++;
+        }
+        for(int i=0;i<b.size();i++){
+            if(mp[b[i]]>0){
+                mp[b[i]]--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> a1, a2;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a1.push_back(number);
+        }
+        getline(cin, input);
+        stringstream ss2(input);
+        while (ss2 >> number) {
+            a2.push_back(number);
+        }
+        Solution s;
+        bool ans = s.isSubset(a1, a2);
+        if (ans) {
+            cout << "true"
+                 << "\n";
+        } else {
+            cout << "false"
+                 << "\n";
+        }
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
