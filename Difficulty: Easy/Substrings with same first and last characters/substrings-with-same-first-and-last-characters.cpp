0e306765144505986a333eba58c51f6c85100dcb
Durga sri niharika <142823558+Durgasriniharika@users.noreply.github.com>
2025-05-21 23:23:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Substrings with same first and last characters/substrings-with-same-first-and-last-characters.cpp
+++ Difficulty: Easy/Substrings with same first and last characters/substrings-with-same-first-and-last-characters.cpp
@@ -1,13 +1,7 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     int countSubstring(string &s) {
+        // code here
         unordered_map<char,int>mp;
         int cnt=0;
         
@@ -19,30 +13,3 @@ class Solution {
         return cnt+s.size();
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int t;
-    cin >> t;
-    while (t--) {
-        string str;
-        cin >> str;
-
-        Solution ob;
-
-        cout << ob.countSubstring(str) << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-
-    return 0;
-}
-// } Driver Code Ends
